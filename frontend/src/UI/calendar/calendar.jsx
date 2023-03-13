c979c02bc116cf7c05b3e04ff03c649f1edcbce9
Artemis Tran <artemistranhb@gmail.com>
2023-03-13 04:33:27 -0700

    Added the ability for users to create events in the calendar

--- frontend/src/UI/calendar/calendar.jsx
+++ frontend/src/UI/calendar/calendar.jsx
@@ -1,4 +1,4 @@
-import React, { Component,useState } from "react";
+import React, { Component,useState, useCallback } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -9,20 +9,25 @@ const events = [
   {
       title: "Workout",
       allDay: true,
-      start: new Date(2023, 2, 1),
-      end: new Date(2023, 2, 1),
+      start: new Date(),
+      end: new Date(),
   },
 ];
 
-
-
 function App() {
-  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  // const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
   const [allEvents, setAllEvents] = useState(events);
 
-  // function handleAddEvent () {
+  const handleAddEvent = useCallback(
+    ({ start, end }) => {
+      const title = window.prompt('New Event name')
+      if (title) {
+        setAllEvents((prev) => [...prev, { start, end, title }])
+      }
+    },
+    // [setAllEvents]
+  )
 
-  // }
 
   return (
     <div className="App">
@@ -31,6 +36,8 @@ function App() {
         defaultDate={new Date()}
         defaultView="month"
         events={allEvents}
+        selectable
+        onSelectSlot={handleAddEvent}
         style={{ height: "100vh" }}
       />
     </div>
