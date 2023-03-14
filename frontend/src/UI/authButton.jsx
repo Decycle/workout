c445953a2277a7d263965da981367be3ed1bc21b
Zimin(Dennis) Yang <Decycle.yang@gmail.com>
2023-03-14 00:43:38 -0700

    hides backend's .env file because openai auto rotates leaked api keys

--- frontend/src/UI/authButton.jsx
+++ frontend/src/UI/authButton.jsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { Logout } from '@mui/icons-material'
 import {
   Avatar,
   Chip,
@@ -95,7 +96,8 @@ const UserMenu = () => {
               },
             })
           }}>
-          Logout
+          <Logout />
+          Log out
         </MenuItem>
       </Menu>
     </div>
